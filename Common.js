2d95e2069f5ed155b903e65e7ded3b5f7ddd2563
Tomas Kavaliauskas <tokavaliauskas@gmail.com>
2024-12-15 21:07:03 +0200

    Create Common.js

---
+++ Common.js
@@ -0,0 +1,12 @@
+exports.sleep = async function(seconds)
+{
+
+    return new Promise(resolve => setTimeout(resolve, seconds*1000));
+
+}
+
+exports.getRandomInt = function (min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
